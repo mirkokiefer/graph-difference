26f53fc73231fffa378753a6eaef21678d3a1b90
Mirko Kiefer <mail@mirkokiefer.com>
2013-08-11 18:32:56 +0200

    refactor test, add more

--- test.js
+++ test.js
@@ -26,12 +26,18 @@ var readParents = function(id, cb) {
   cb(null, nodes[id])
 }
 
+var tests = [
+  {from: 7, to: 9, expected: [9, 8]},
+  {from: 6, to: 7, expected: [7]}
+]
+
 describe('tree-difference', function() {
-  it('should find the subtree that connects one node with another', function(done) {
-    var expected = [9, 8]
-    treeDiff(7, 9, readParents, function(err, res) {
-      assert.deepEqual(res, expected)
-      done()
+  tests.forEach(function(each, i) {
+    it('should run test ' + i, function(done) {
+      treeDiff(each.from, each.to, readParents, function(err, res) {
+        assert.deepEqual(res, each.expected)
+        done()
+      })
     })
   })
 })
